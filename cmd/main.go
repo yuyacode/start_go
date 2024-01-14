0ff6b5f5030f87a893ec2684ef62e9b2e27a9b83
長塚裕也 <ny410144@gmail.com>
2024-01-14 13:28:50 +0900

    他パッケージをインポートして利用してみる

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"./animals"
 )
 
 func main() {
 	fmt.Println("Hello Go!")
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
 }
