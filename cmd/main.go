534ac10b26491646bfa9e8192ce27124a0b54f1f
長塚裕也 <ny410144@gmail.com>
2024-01-15 16:08:04 +0900

    Goでは、目に見えないが、行末には;が存在するらしい
    コンパイル時に、{や,などを除いて、行末に;が追加される？みたい。
    なので配列の要素を改行して整形した際には、最後の要素に,を忘れないように。,を書かないと、行末だと判断され;が付与されエラーになる

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,18 @@ import (
 
 func main() {
 	// fmt.Println("Hello Go!")
+
 	// fmt.Println(animals.ElephantFeed())
 	// fmt.Println(animals.MonkeyFeed())
 	// fmt.Println(animals.RabbitFeed())
 	
-	fmt.Println(AppName())
+	// fmt.Println(AppName())
+
+	a := [3] string {
+		"Taro",
+		"Hanako",
+		"Kenji",
+	}
+
+	fmt.Println(a)
 }
