0eac192e1ba2d6d2fdb40e5c95d7d617a673ab53
長塚裕也 <ny410144@gmail.com>
2024-01-25 10:09:24 +0900

    関数が複数の戻り値を返す場合

--- cmd/main.go
+++ cmd/main.go
@@ -226,5 +226,10 @@ func main() {
    // fmt.Println(^int8(16))  // -17
    // fmt.Println(^uint8(16))  // 239
 
-   fmt.Println(plus(51, 37))
+   // fmt.Println(plus(51, 37))
+
+	// hello()
+
+	q, r := div(10, 3)
+	fmt.Printf("%d余り%d\n", q, r)
 }
--- cmd/plus.go
+++ cmd/plus.go
@@ -1,7 +1,21 @@
 package main
 
-// import ()
+import (
+	"fmt"
+)
 
 func plus(x, y int) int {
 	return x + y
+}
+
+func hello() {
+	fmt.Println("Hello GO")
+}
+
+// 戻り値の型は１つに省略できないみたい。単にintと定義すると、単一の整数値のみを返す関数と認識されてしまうみたい
+// (int, int)をint, intではコンパイルエラーになる。()は必須
+func div(a, b int) (int, int) {
+	q := a / b
+	r := a % b
+	return q, r
 }
