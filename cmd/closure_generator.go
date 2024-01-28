386b94da732bb6a70a9db2f706e4b08f6b076795
長塚裕也 <ny410144@gmail.com>
2024-01-28 15:10:23 +0900

    クロージャを使ったジェネレータの実装

---
+++ cmd/closure_generator.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ints := integers()
+
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+
+	// クロージャを別に新しく生成
+	// クロージャ間で状態は共有されない
+	otherInts := integers()
+	fmt.Println(otherInts())
+}
+
+func integers() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
