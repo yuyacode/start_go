9b56384aaa6dedb1147c2d0029509f8063b906da
長塚裕也 <ny410144@gmail.com>
2024-02-01 06:09:46 +0900

    関数のスコープ
    名前付き戻り値についてや、関数内により深いブロックを定義できることを知った
    関数内により深いブロックを定義できる体験ははじめて。他言語ではなかった

--- cmd/foo/scope.go
+++ cmd/foo/scope.go
@@ -18,3 +18,37 @@ func internalFunc(n int) int {
 }
 
 // 日本語など大文字、小文字のない文字の場合、小文字と同様に他パッケージから参照できない
+
+func sum(a, b int) (result int) {
+	// var result int や result := a + b のように再度宣言する必要はない
+	// 'result' はすでに定義されているので、直接値を代入できる
+    result = a + b
+    return          // 'return' のみで 'result' の現在の値が返される
+}
+
+// func bar(a int) (b string) {
+// 	var a int           // aは定義済みなのでエラーになる
+// 	const b = "string"  // bは定義済みなのでエラーになる
+// 	return b
+// }
+
+func Hoge() (b string) {  // そもそも識別子の重複は避けるべき
+	b = "string"
+	{
+		// 関数より深いブロック
+		const b = "str"  // ブロック内でのみ有効な定数
+		// 中略
+	}
+	return b  // string
+}
+
+func Biz() (b string) {
+	b = "string"
+	{
+		// 関数より深いブロック
+		const b = "str"
+		return          // return文は関数レベルでのみ有効であり、ブロックレベルでは許可されていない。コンパイルエラー
+		// 中略
+	}
+	return  // 外に出す
+}
