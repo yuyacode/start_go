7355bae811ae3b2dc4c0dfe48a3636cefd5e4d39
長塚裕也 <ny410144@gmail.com>
2024-02-17 14:57:58 +0900

    １つのパッケージ内に複数のinit関数がある場合の挙動
    実行順序をはじめとした挙動の詳細は、あまり分かっていない。
    ただ、そもそも１つのパッケージに複数のinit関数が存在することは、多分あまりないケースだと思うので、まあ一旦は理解しなくて良いのではないかと感じる。
    複雑な初期化ロジックや、複数のinit関数間での依存関係が生じるような場合は、代わりに明示的な初期化関数を用意し、それをmain関数から呼び出すことが推奨されているみたい。

---
+++ cmd/init_1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("init_1")
+}
+
+func sample1() {
+	fmt.Println("sample1")
+}
---
+++ cmd/init_2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("init_2")
+}
+
+func sample2() {
+	fmt.Println("sample2")
+}
